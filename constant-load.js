1df1dae36a1aaf2b1c798316afcba1147d0af791
sherrerah3 <sherrerah3@eafit.edu.co>
2026-09-07 22:57:25 -0500

    Add k6 load test

---
+++ constant-load.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+const RATE = Number(__ENV.RATE || 5);
+const BASE_URL = __ENV.BASE_URL || 'http://3.89.55.234:8080';
+const ITERATIONS = Number(__ENV.ITERATIONS || 500000);
+
+export const options = {
+  scenarios: {
+    fixed_rate: {
+      executor: 'constant-arrival-rate',
+      rate: RATE,
+      timeUnit: '1s',
+      duration: '3m',
+      preAllocatedVUs: RATE * 2 + 10,
+      maxVUs: RATE * 10 + 50,
+    },
+  },
+};
+
+export default function () {
+  const res = http.get(`${BASE_URL}/work?iterations=${ITERATIONS}`);
+  
+  check(res, {
+    'status 200': (r) => r.status === 200,
+  });
+}
